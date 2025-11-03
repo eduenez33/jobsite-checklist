dde625f49c097f10cebc60e849298d6da5262c2a
Edwin Duenez <duenezedwin33@gmail.com>
2025-11-03 16:40:36 -0700

    add Header component with layout and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import "./Header.css";
 
 function Header() {
-  return <h1>Hello World!</h1>;
+  return (
+    <header className="header">
+      <div className="header__text">
+        <h1 className="header__title">Listo.</h1>
+        <p className="header__caption">
+          Manage your construction sites and supply checklists
+        </p>
+      </div>
+      <button className="header__button">
+        + <span>Create New Site</span>
+      </button>
+    </header>
+  );
 }
 
 export default Header;
